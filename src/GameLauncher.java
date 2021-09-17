5222c0773d213ae2726e517eebe8aedca1aad93e
CamrenCarter2 <70165583+CamrenCarter2@users.noreply.github.com>
2021-09-16 20:35:42 -0500

    Initial commit

---
+++ src/GameLauncher.java
@@ -0,0 +1,13 @@
+/* Camren Carter
+ * 9/16/2021
+ * CSC205
+ */
+public class GameLauncher {
+
+	public static void main(String[] args) {
+		 GuessGame game = new GuessGame();
+		 game.startGame();
+
+	}
+
+}
